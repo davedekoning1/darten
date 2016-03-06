65272fe134e256e051b47eca8e17688ff1d9ca23
davedekoning1 <davedekoning1@gmail.com>
2016-03-06 22:40:38 +0100

    Update of the catalogue/database reader

--- database_catalogue.py
+++ database_catalogue.py
@@ -17,30 +17,198 @@ class AutoVivification(dict):
             
 class overview:
     
-    def __init__(self):
+    frame_defaults = {'rowspan': 1,
+                    'columnspan': 1}
+                            
+    listbox_defaults = {'rowspan': 1,
+                        'columnspan': 1,
+                        'width': 10}
+                        
+    labels_defaults = {'rowspan': 1,
+                        'columnspan': 1,
+                        'width': 10,
+                        'var': False,
+                        'textvariable': None,
+                        'text': None}
+    
+    def check_defaults(self, defaults, objects):
+        for frame in objects.values():
+            for default in defaults.keys():
+                try:
+                    frame[default]
+                except:
+                    frame[default] = defaults[default]
+    
+    def __init__(self):   
+        
         self.__mainWindow = Tk()
         
-        ## 
+        ### Frames ###
+        self.Frames = {'top_frame': {'name': 'top_frame',
+                                'parent': self.__mainWindow,
+                                'row': 1,
+                                'column': 1},
+                  'match_overview': {'name': 'match_overview',
+                                'parent': self.__mainWindow,
+                                'row': 2,
+                                'column': 1},
+                  'player_overview': {'name': 'player_overview',
+                                'parent': self.__mainWindow,
+                                'row': 2,
+                                'column': 2},
+                  'graph_frame': {'name': 'graph_frame',
+                                'parent': self.__mainWindow,
+                                'row': 1,
+                                'column': 2,
+                                'rowspan': 1}}     
+        self.frames = {}
+        self.listboxes = {}
+        self.labels = {}
+        
+        self.check_defaults(self.frame_defaults, self.Frames)
+        
+        for frame in self.Frames.values():
+            self.frames[frame['name']] = self.create_frame(frame['parent'], frame['row'], frame['column'], frame['rowspan'], frame['columnspan'])
+        
+        ### Listboxes ###
+        self.Listboxes = {'match_box': {'name': 'match_box',
+                                    'parent': self.frames['match_overview'],
+                                    'row': 1,
+                                    'column': 1,
+                                    'width': 50}}
+        
+        self.check_defaults(self.listbox_defaults, self.Listboxes)
+        
+        for listbox in self.Listboxes.values():
+            self.listboxes[listbox['name']] = self.create_listbox(listbox['parent'], listbox['row'], listbox['column'], listbox['rowspan'], listbox['columnspan'], listbox['width'])
+        
+        ### Menu ###
+        var = StringVar()
+        players = data_load['player_list']
+        menu = OptionMenu(self.frames['top_frame'], var, *players)
+        menu.grid(row = 1, column = 1)
+        var.set(players[0])
+        
+        self.getting_playerstats(players[0], data_load) ## Zorg ervoor dat hier de speler komt die geselecteerd is in het menu.
+        self.filling_listbox(self.listboxes['match_box'])
+        
+        ### Labels ###
+        self.Labels = {'av_last_match': {'name': 'av_last_match',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 1,
+                                        'text': 'Last match average'},
+                        'av_last_match_val': {'name': 'av_last_match_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 2,
+                                        'var': True,
+                                        'textvariable': self.av_last_match},
+                        'result_last_match': {'name': 'result_last_match',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 3,
+                                        'var': True,
+                                        'textvariable': self.result_last_match},
+                        'highest_fin': {'name': 'highest_fin',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 1,
+                                        'text': 'Highest finish'},
+                        'highest_fin_val': {'name': 'highest_fin_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 2,
+                                        'var': True,
+                                        'textvariable': self.highest_fin},
+                        'highest_av': {'name': 'highest_av',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 3,
+                                        'text': 'Highest average'},
+                        'highest_av_val': {'name': 'highest_av_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 4,
+                                        'var': True,
+                                        'textvariable': self.highest_av},
+                        'highest_throw': {'name': 'highest_throw',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 5,
+                                        'text': 'Highest throw'},
+                        'highest_throw_val': {'name': 'highest_throw_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 6,
+                                        'var': True,
+                                        'textvariable': self.highest_throw},
+                                    }
+                                    
+        self.check_defaults(self.labels_defaults, self.Labels)
+        for label in self.Labels.values():
+            self.labels[label['name']] = self.create_labels(label['parent'], label['row'], label['column'], label['rowspan'], label['columnspan'], label['width'], label['var'], label['textvariable'], label['text'])
+        
+        mainloop()
+        
+    def create_frame(self, parent, row, column, rowspan, columnspan):
+        new_frame = Frame(parent)
+        new_frame.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan)
+        return new_frame
+    
+    def create_listbox(self, parent, row, column, rowspan, columnspan, width):
+        new_listbox = Listbox(parent, width = width)
+        new_listbox.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan)
+        return new_listbox
+        
+    def create_labels(self, parent, row, column, rowspan, columnspan, width, var, textvariable, text):
+        
+        if var == True:
+            textvar = StringVar()
+            textvar.set(textvariable)
+            new_label = Label(parent, textvariable = textvar)
+        else:
+            print text
+            new_label = Label(parent, text = text)
+        
+        new_label.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan, sticky = EW)
+        
+        return new_label
+
+    def filling_listbox(self, listbox):
+        for item in self.list_of_strings:
+            listbox.insert(END, item)
     
     ## filling player overview
-    def Filling_Overview(player, data):
-        name = player.name
-        
+    def getting_playerstats(self, name, data):
+        all_matches = []
         all_throws = []
         all_averages = []
         all_dates = []
-        list_of_strings = []
-        
+        all_finishes = []
+        self.list_of_strings = []
+    
         for match in data['matches'].values():
             if name in match.players:
                 name_opp = [item for item in match.players if item != name][0]
                 all_matches.append(match)
-                all_throws += match.throws[name]
-                all_averages.append(np.mean(match.throws[name]))
-                all_dates.append(match.date_tag) ## stull need s to be added to the darten_gui
-                
-                string = "{:8s}{:15s}{:15s}{:15s}{:4i}{:4i}".format((match.match_id, datetime.strftime(match.date_tag,"%Y-%m-%d %H:%M"), name, name_opp, match.bo_sets, match.bo_legs))
-                list_of_strings.append()
+                all_throws += match.match_throws[name]
+                all_averages.append(np.mean(match.match_throws[name]))
+                all_dates.append(match.date_tag)
+                all_finishes.append(12)
+            
+                string = '{:<4d}{:<20s}{:<10s}{:<10s}{:>3d}{:>3d}'.format(match.match_id, datetime.strftime(match.date_tag,"%Y-%m-%d %H:%M"), name, name_opp, match.bo_sets, match.bo_legs)
+                self.list_of_strings.append(string)
+        
+        self.highest_fin = np.max(all_finishes)   
+        self.highest_av = np.max(all_averages)
+        self.highest_throw = np.max(all_throws)
+        self.av_last_match = all_averages[-1]
+        if name == match.won_by:
+            self.result_last_match = 'win'
+        else:
+            self.result_last_match = 'lose'
+
                 
 def Filling_Overview(name, data):
     
@@ -74,7 +242,9 @@ if __name__ == "__main__":
         
     players = data_load['player_list']
     
-    print players
-    
-    for player in players:
-        print Filling_Overview(player, data_load)
+    # print players
+#
+#     for player in players:
+#         print Filling_Overview(player, data_load)
+        
+    overview = overview()
