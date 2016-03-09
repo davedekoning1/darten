020e10c1378f2651c8c63fe505cbde148b766daa
davedekoning1 <davedekoning1@gmail.com>
2016-03-09 20:48:36 +0100

    added more to the playerview

--- database_catalogue.py
+++ database_catalogue.py
@@ -54,11 +54,11 @@ class overview:
                                 'column': 1},
                   'player_overview': {'name': 'player_overview',
                                 'parent': self.__mainWindow,
-                                'row': 2,
+                                'row': 1,
                                 'column': 2},
                   'graph_frame': {'name': 'graph_frame',
                                 'parent': self.__mainWindow,
-                                'row': 1,
+                                'row': 2,
                                 'column': 2,
                                 'rowspan': 1}}     
         self.frames = {}
@@ -83,11 +83,13 @@ class overview:
             self.listboxes[listbox['name']] = self.create_listbox(listbox['parent'], listbox['row'], listbox['column'], listbox['rowspan'], listbox['columnspan'], listbox['width'])
         
         ### Menu ###
-        var = StringVar()
+        self.var = StringVar(self.frames['top_frame'])
         players = data_load['player_list']
-        menu = OptionMenu(self.frames['top_frame'], var, *players)
+        menu = OptionMenu(self.frames['top_frame'], self.var, *players)
         menu.grid(row = 1, column = 1)
-        var.set(players[0])
+        self.var.set(players[0])
+        self.selected_player = players[0]
+        self.var.trace("w", lambda *args: self.option_changed(self.var))
         
         self.getting_playerstats(players[0], data_load) ## Zorg ervoor dat hier de speler komt die geselecteerd is in het menu.
         self.filling_listbox(self.listboxes['match_box'])
@@ -151,6 +153,68 @@ class overview:
         
         mainloop()
         
+    def option_changed(self, varia):
+        self.selected_player = varia.get()
+        self.getting_playerstats(varia.get(), data_load) 
+        self.filling_listbox(self.listboxes['match_box'])
+        
+        self.Labels = {'av_last_match': {'name': 'av_last_match',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 1,
+                                        'text': 'Last match average'},
+                        'av_last_match_val': {'name': 'av_last_match_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 2,
+                                        'var': True,
+                                        'textvariable': self.av_last_match},
+                        'result_last_match': {'name': 'result_last_match',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 1,
+                                        'column': 3,
+                                        'var': True,
+                                        'textvariable': self.result_last_match},
+                        'highest_fin': {'name': 'highest_fin',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 1,
+                                        'text': 'Highest finish'},
+                        'highest_fin_val': {'name': 'highest_fin_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 2,
+                                        'var': True,
+                                        'textvariable': self.highest_fin},
+                        'highest_av': {'name': 'highest_av',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 3,
+                                        'text': 'Highest average'},
+                        'highest_av_val': {'name': 'highest_av_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 4,
+                                        'var': True,
+                                        'textvariable': self.highest_av},
+                        'highest_throw': {'name': 'highest_throw',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 5,
+                                        'text': 'Highest throw'},
+                        'highest_throw_val': {'name': 'highest_throw_val',
+                                        'parent': self.frames['player_overview'],
+                                        'row': 2,
+                                        'column': 6,
+                                        'var': True,
+                                        'textvariable': self.highest_throw},
+                                    }
+        
+        self.check_defaults(self.labels_defaults, self.Labels)
+        for label in self.Labels.values():
+            self.labels[label['name']] = self.create_labels(label['parent'], label['row'], label['column'], label['rowspan'], label['columnspan'], label['width'], label['var'], label['textvariable'], label['text'])
+        
+        
     def create_frame(self, parent, row, column, rowspan, columnspan):
         new_frame = Frame(parent)
         new_frame.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan)
@@ -159,6 +223,7 @@ class overview:
     def create_listbox(self, parent, row, column, rowspan, columnspan, width):
         new_listbox = Listbox(parent, width = width)
         new_listbox.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan)
+        new_listbox.bind('<<ListboxSelect>>', self.onselect)
         return new_listbox
         
     def create_labels(self, parent, row, column, rowspan, columnspan, width, var, textvariable, text):
@@ -168,7 +233,6 @@ class overview:
             textvar.set(textvariable)
             new_label = Label(parent, textvariable = textvar)
         else:
-            print text
             new_label = Label(parent, text = text)
         
         new_label.grid(row = row, column = column, rowspan = rowspan, columnspan = columnspan, sticky = EW)
@@ -176,8 +240,78 @@ class overview:
         return new_label
 
     def filling_listbox(self, listbox):
+        listbox.delete(0,END)
         for item in self.list_of_strings:
             listbox.insert(END, item)
+            
+    def onselect(self, evt):
+        w = evt.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        
+        self.selected_match_id = value.split()[0]
+        
+        self.selected_match = data_load['matches'][int(self.selected_match_id)]
+        self.opponent = [player for player in self.selected_match.players if player != self.selected_player][0]
+        
+        all_sets = []
+        all_legs = []
+        row = 1
+        
+        self.label_dict = {'top1': {'name': 'top1',
+                                        'parent': self.frames['graph_frame'],
+                                        'row': row,
+                                        'column': 1,
+                                        'text': ''},
+                        'top2': {'name': 'top2',
+                                        'parent': self.frames['graph_frame'],
+                                        'row': row,
+                                        'column': 2,
+                                        'var': True,
+                                        'textvariable': self.selected_player},
+                        'top3': {'name': 'top3',
+                                        'parent': self.frames['graph_frame'],
+                                        'row': row,
+                                        'column': 3,
+                                        'var': True,
+                                        'textvariable': self.opponent}}
+        
+        for sets in data_load['sets'].values():
+            if sets.set_id.split('.')[0] == self.selected_match_id:
+                all_sets.append(sets)
+        
+        f_set_id = 1
+        for leg in data_load['legs'].values():
+            if leg.leg_id.split('.')[0] == self.selected_match_id:
+                all_legs.append(leg)
+                leg_average_player = '{:.2f}'.format(np.mean(leg.leg_throws[self.selected_player]))
+                leg_average_oppo = '{:.2f}'.format(np.mean(leg.leg_throws[self.opponent]))
+                c_set_id = int(leg.leg_id.split('.')[1])
+                c_leg_id = int(leg.leg_id.split('.')[-1])
+                row += 1
+                if c_set_id > f_set_id:
+                    col = 1
+                    for text in ['', '', '']:
+                        self.add_to_label_dict(col, row, text)
+                        col += 1
+                    row += 1
+                
+                input_string = 'Set %s - Leg %s' % (c_set_id, c_leg_id)
+                col = 1
+                for text in [input_string, leg_average_player, leg_average_oppo]:
+                    self.add_to_label_dict(col, row, text)
+                    col += 1
+        
+        print self.label_dict
+        
+        self.check_defaults(self.labels_defaults, self.label_dict)
+        for label in self.label_dict.values():
+            self.labels[label['name']] = self.create_labels(label['parent'], label['row'], label['column'], label['rowspan'], label['columnspan'], label['width'], label['var'],label['textvariable'], label['text'])
+    
+    def add_to_label_dict(self, col, row, text):
+        c_frame = self.frames['graph_frame']
+        name = col + (row - 1) * 3
+        self.label_dict[name] = dict(name=name, parent=c_frame, row=row, column=col, text=text)
     
     ## filling player overview
     def getting_playerstats(self, name, data):
@@ -186,6 +320,10 @@ class overview:
         all_averages = []
         all_dates = []
         all_finishes = []
+
+        all_sets = []
+        all_legs = []
+        
         self.list_of_strings = []
     
         for match in data['matches'].values():
@@ -195,11 +333,20 @@ class overview:
                 all_throws += match.match_throws[name]
                 all_averages.append(np.mean(match.match_throws[name]))
                 all_dates.append(match.date_tag)
-                all_finishes.append(12)
             
                 string = '{:<4d}{:<20s}{:<10s}{:<10s}{:>3d}{:>3d}'.format(match.match_id, datetime.strftime(match.date_tag,"%Y-%m-%d %H:%M"), name, name_opp, match.bo_sets, match.bo_legs)
                 self.list_of_strings.append(string)
         
+        for match in all_matches:
+            for sets in data['sets'].values():
+                if sets.set_id.split('.')[0] == str(match.match_id):
+                    all_sets.append(sets)
+            for leg in data['legs'].values():
+                if leg.leg_id.split('.')[0] == str(match.match_id):
+                    all_legs.append(leg)
+                    if leg.won_by == self.selected_player:
+                        all_finishes.append(leg.finish)
+                    
         self.highest_fin = np.max(all_finishes)   
         self.highest_av = np.max(all_averages)
         self.highest_throw = np.max(all_throws)
